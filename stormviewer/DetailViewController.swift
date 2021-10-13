4b0d13ee5cbdea3d124e3498170824894a73c560
Karina Martins <karinas@brq.com>
2021-10-13 18:49:44 -0300

    part 1/2 of project1

---
+++ stormviewer/DetailViewController.swift
@@ -0,0 +1,20 @@
+//
+//  DetailViewController.swift
+//  stormviewer
+//
+//  Created by Karina Martins Silva on 13/10/21.
+//
+
+import UIKit
+
+class DetailViewController: UIViewController {
+
+    @IBOutlet var imageView: UIImageView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+}
